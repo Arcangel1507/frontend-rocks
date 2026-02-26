2c2a0298c6065263f97f9953e16e4cd06ef6b813
Arcangel1507 <carloslucho1508@gmail.com>
2026-02-26 17:00:12 +0000

    aggiornamento vite.config.ts

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,43 +3,47 @@ import { Link } from "react-router";
 import { PokeAPI } from "./api";
 */
 
-import { useEffect } from "react";
-import { PokeAPI } from "./api";
-
-  type pokemon= {
+interface Props {
   id: number;
   image: string;
   name: string;
   types: string[];
 }
-   function card(prop: Props)
-    function sum
-export function root() {
-  const [offset, setCount]
-  const [pokemons, setPokemons] = useState<Pokemon[]>([
-    {
-    id: 0,
-    Image: "",
-    name: "Pikachu",
-    types: ["electric"]
-    }
-  ])
-  useEffect(() => {
-    PokeAPI.listpokemons(offset).then(result => {
-      setPokemons ( state => ([
-        ...state,
-        ...result.results.map((item ) =>)
-      ]))
-    })
-  })
-}
-function loadMore() {
-  setPokemons((state)=> ({
-    ...state
-  }
-}
-function loadMore(){
-setCount(state => state + 20)
+
+export const Card: React.FC<Props> = (props) => (
+  <div className="bg-white border border-gray-300 rounded-lg shadow-md relative w-2xs flex items-center justify-center h-80">
+    <h4 className="text-xl text-gray-900 tracking-wide font-bold absolute left-4 top-2">
+      {props.name} -{" "}
+      <span className="text-gray-700 font-medium">{props.id}</span>
+    </h4>
+
+    <img
+      src={props.image}
+      alt={props.name}
+      className="w-36 h-36 object-contain"
+    />
+
+    <div className="text-sm text-gray-700 absolute right-2 bottom-2">
+      <div className="flex justify-center space-x-2">
+        {props.types.map((type) => (
+          <span
+            key={type}
+            className={`font-bold text-white px-3 py-1 rounded-md text-xs ${getTypeColor(type)}`}
+          >
+            {type}
+          </span>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+export function Root() {
+  return <Card
+    id={0}
+    image="https://media.pokemoncentral.it/wiki/thumb/5/5e/Artwork0025_RFVF.png/800px-Artwork0025_RFVF.png"
+    name="Pikachu"
+    types={["electric"]}
+    />
 }
 function getTypeColor(type: string): string {
   return typeColors[type];
